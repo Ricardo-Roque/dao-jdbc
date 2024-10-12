54c9ec9b469bbe584fe4fff91d94ee77ce26083e
ricardo.roque <ricardo.roque@neomind.com.br>
2024-10-11 21:42:48 -0300

    insert implementation

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -6,6 +6,7 @@ import org.example.model.dao.SellerDAO;
 import org.example.model.entities.Department;
 import org.example.model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -31,6 +32,13 @@ public class Program {
         for (Seller obj : sellerList) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
 
     }
 }
